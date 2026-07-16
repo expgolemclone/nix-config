a23cff35b59f6961df369c39f1cc590fea3a4087
exp <108988326+expgolemclone@users.noreply.github.com>
2026-07-17 02:43:11 +0900

    Harden external HDD initrd boot

---
+++ modules/external-hdd-backup-initrd.nix
@@ -0,0 +1,20 @@
+{ lib, ... }:
+
+{
+  specialisation.external-hdd-backup.configuration = {
+    # The backup root lives behind a USB-SATA bridge.  Make the complete
+    # storage path available before udev-based coldplug and force it to load.
+    boot.initrd.kernelModules = [
+      "xhci_pci"
+      "sd_mod"
+      "uas"
+      "usb_storage"
+    ];
+
+    # Large external HDDs can take longer than systemd's default device
+    # timeout to spin up and expose their partition UUIDs.
+    fileSystems."/".options = lib.mkForce [
+      "x-systemd.device-timeout=5min"
+    ];
+  };
+}
